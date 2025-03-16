28b703e2c68c9f6577236b5292fbe4dbe8ab6a6b
skgnandy <skgnandy@gmail.com>
2025-03-16 12:47:00 +0530

    pagination added in blog api

--- src/api/blog/controllers/blog.js
+++ src/api/blog/controllers/blog.js
@@ -10,8 +10,12 @@ module.exports = createCoreController('api::blog.blog', ({ strapi }) => ({
     async find(ctx) {
         try {
             const { query } = ctx;
+            const pagination = ctx.query?.pagination;
             const entries = await strapi.entityService.findMany('api::blog.blog', {
                 ...query,
+                sort: { createdAt: "desc" },
+                start: pagination?.start ? pagination?.start : 0,
+                limit: pagination?.limit ? pagination?.limit : 20
             });
             return this.transformResponse(entries);
         } catch (err) {
